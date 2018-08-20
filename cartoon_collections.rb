d16b3b04972dde87129094e9b788d68146a08cf4
Thomas Pope <tpope0928@gmail.com>
2018-08-20 15:41:02 +0000

    automatically backed up by learn

---
+++ cartoon_collections.rb
@@ -0,0 +1,21 @@
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, index| puts "#{index +1}. #{dwarf}"}
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
+end
+
+def long_planteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
+  call.length > 4
+  end
+end
+
+def find_the_cheese(snacks)
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find do |item|
+    cheese_types.include?(item)
+  end
+end
